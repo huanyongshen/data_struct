85a5f89f213352eb6528915c7ee10c4dbfa42e23
root <root@localhost.localdomain>
2017-03-27 23:05:58 +0800

    add list define and implement

---
+++ list.c
@@ -0,0 +1,60 @@
+#include "list.h"
+
+static void copy_to_node(ITEM *pnew, ITEM *pold)
+{
+	memcpy(pnew, pold, sizeof(ITEM));
+}
+
+void list_init(List *plist)
+{
+	if (plist) {
+		memset(plist, 0, sizeof(List));
+	}
+}
+
+BOOL list_is_empty(List *plist)
+{
+	if (plist == NULL) {
+		return TRUE;
+	}
+	
+	return plist->count ? FALSE : TRUE;
+}
+
+BOOL list_is_full(List *plist)
+{
+	Node *node = NULL;
+
+	node = (Node *)malloc(sizeof(Node));
+	if (node == NULL) {
+		return TRUE;	
+	}
+	
+	free(node);
+
+	return FALSE;
+}
+
+BOOL list_add(List *plist, Node *pnode)
+{
+	if (list_is_full(plist)) {
+		printf("No memory available\n");
+		return FALSE;
+	}
+
+	Node *node = (Node *)malloc(sizeof(Node));
+	if (node == NULL) {
+		printf("malloc node failed\n");
+		return FALSE;
+	}
+
+	
+
+	if (plist == NULL) {
+		plist->next = node;
+		node->next = NULL;
+	} else {
+		node->next = plist->next;
+	}
+}
+
---
+++ list.h
@@ -0,0 +1,47 @@
+#ifndef __LIST_H_
+#define __LIST_H_
+
+typedef enum bool_em {
+	FALSE = 0,
+	TREU = 1
+} BOOL;
+
+#define MAX_NAME_LEN 128
+
+typedef struct person_st {
+	char name[MAX_NAME_LEN];
+	int age;
+} ITEM;
+
+typedef struct node_st {
+	ITEM item;
+	struct node_st *next;	
+} Node; 
+
+typedef struct list_st {
+	Node *node;	
+	int count;
+} List;
+
+// init list 
+void list_init(List *plist);
+
+// list is empty
+BOOL list_is_empty(List *plist);
+
+// list is full
+BOOL list_is_full(List *plist);
+
+// list add 
+BOOL list_add(List *plist, Node *pnode);
+
+// list delete
+BOOL list_delete(List *plist, Node *pnode);
+
+// list show
+void list_show(List *plist); 
+
+// list find 
+BOOL list_find(List *plist, Node *pnode);
+
+#endif
